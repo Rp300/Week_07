55eb87b2f2ec8e5a75de7ce6b6a695ea5a8c3256
Rp300 <38594639+Rp300@users.noreply.github.com>
2018-10-17 20:02:34 -0500

    updates

--- Assignment.js
+++ Assignment.js
@@ -4,6 +4,17 @@ const app = express()
 
 const port = 3000
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
+app.get("/:page", (req, res) => {
+    let page = req.params.page
+    switch(page) {
+        case "home":
+            res.send("home")
+            break;
+        case "about":
+            res.send("about")
+            break;
+        case "info":
+            res.send("info")
+            break;
+    }
   })
